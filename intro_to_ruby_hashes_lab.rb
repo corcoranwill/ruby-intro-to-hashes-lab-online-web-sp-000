e1ef14aa98bbfd8b5d00318951972804b58426fb
Will Corcoran <corcoranwill@gmail.com>
2020-02-22 12:22:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,17 +12,20 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-  monopoly = {
-    :railroads => {
-      :pieces => 4,
-      :rent_in_dollars => {
-
+  monopoly =  {
+    :railroads=>
+    {
+      :pieces=>4,
+      :rent_in_dollars=>
+      {
+    
       },
-      :names => {
+      :names=>
+      {
 
-      }
       }
     }
+  }
 end
 
 def monopoly_with_third_tier
